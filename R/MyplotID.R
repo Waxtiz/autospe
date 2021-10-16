4bbc1aeffdbded55308c2f1dd0babcbd4d4e4a19
Waxtiz <92598851+Waxtiz@users.noreply.github.com>
2021-10-16 13:26:44 +0200

    0.1.1

--- R/MyplotID.R
+++ R/MyplotID.R
@@ -3,7 +3,7 @@
 #' Créer un graphique de la raréfaction - calculé à l'aide du package vegan - et de la richesse spécifique cumulé depuis un jeu de données au format long.
 #' x = les identifiants de VarT
 #'
-#' @return data.frame en format "wide"
+#' @return graphique des courbes de la richesse cumulé et de la raréfaction classé par les identifiants
 #'
 #'
 #' @param dataset Jeu de données en format long / data.frame
@@ -16,6 +16,11 @@
 #' @importFrom ggplot2 geom_ribbon
 #' @importFrom ggplot2 aes
 #' @importFrom ggplot2 geom_point
+#' @importFrom ggplot2 scale_color_manual
+#' @importFrom ggplot2 scale_fill_manual
+#' @importFrom ggplot2 scale_x_continuous
+#' @importFrom ggplot2 ylab
+#' @importFrom ggplot2 theme_classic
 #'
 #' @export
 
@@ -24,12 +29,17 @@ My.plot.id <- function(dataset, VarSp, VarT, Occ) {
 
   ggsp2<-My.all(dataset, VarSp, VarT, Occ)
 
-  plot<-ggplot(ggsp2)+
-    geom_line(aes(x=id, y=Rich), color='red')+
-    geom_line(aes(x=id, y=Richness), color='blue',linetype = "dashed")+
-    geom_ribbon(aes(x=id, y=Richness, ymin=(Richness-SD),ymax=(Richness+SD)),alpha=0.1, fill='blue')+
-    geom_point(aes(x=id, y=Richness), color='blue')
+  plot1<-ggplot(ggsp2, aes(x=id))+
+    geom_line(aes(y=Rich, color="Cumulative species richness")) +
+    geom_line(aes(y=Richness, color="Rarefaction"),linetype = "dashed")+
+    geom_point(aes(y=Richness, color="Rarefaction"))+
+    geom_ribbon(aes(y=Richness, ymin=(Richness-SD),ymax=(Richness+SD), fill="SD of rarefaction"),alpha=0.1)+
+    scale_color_manual(name = "", values = c("Rarefaction" = "blue", "Cumulative species richness" = "red"))+
+    scale_fill_manual(name = "", values = c("SD of rarefaction" = "blue")) +
+    scale_x_continuous("Sessions", breaks = ggsp2$id)+
+    ylab("Richness of species")+
+    theme_classic()
 
-  return(plot)
+  return(plot1)
 
 }
